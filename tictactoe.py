9284defc6c21cc74a4d62c7ef30769f45b6eb960
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2017-01-20 12:10:53 +0900

    state reduction

--- tictactoe.py
+++ tictactoe.py
@@ -19,12 +19,46 @@ def board_to_possible_hands(board):
 def init_board():
     return np.zeros(9, dtype=np.int)
 
-def init_Q():
-    return [0] * (3 ** 9 * 9)
+class QRaw(object):
+    def __init__(self):
+        self.value = [0] * (3 ** 9 * 9)
+    def get(self, s, a):
+        return self.value[s * 9 + a]
+    def set(self, s, a, v):
+        self.value[s * 9 + a] = v
+    def board_to_state(self, board):
+        return board_to_int(board)
 
-def policy_random(board):
+
+class QSym(QRaw):
+    "Compress states using symmetry"
+    def board_to_state(self, board):
+        buf = []
+        for i in range(4):
+            buf.append(board_to_int(board))
+            # rotate 90 ccw
+            board = board[[2, 5, 8, 1, 4, 7, 0, 3, 6]]
+        # mirror
+        board = board[[2, 1, 0, 5, 4, 3, 8, 7, 6]]
+        for i in range(4):
+            buf.append(board_to_int(board))
+            # rotate 90 ccw
+            board = board[[2, 5, 8, 1, 4, 7, 0, 3, 6]]
+
+        return min(buf)
+
+
+class QLine(QRaw):
+    "Compress states using symmetry"
+    def board_to_state(self, board):
+        raise NotImplementedError
+        return min(buf)
+
+
+
+def policy_random(env):
     from random import choice
-    actions = board_to_possible_hands(board)
+    actions = board_to_possible_hands(env.board)
     return choice(actions)
 
 LINES = [
@@ -72,7 +106,7 @@ class Environment(object):
             self.result_log.append(0)
             return (self, -1)
 
-        op_action = self.op_policy(self.board)
+        op_action = self.op_policy(self)
         self.board[op_action] = 2
         b = is_win(self.board)
         if b:
@@ -84,20 +118,20 @@ class Environment(object):
 
 
 def play(policy1, policy2=policy_random, to_print=False):
-    board = init_board()
+    env = Environment()
     result = 0
     for i in range(9):
         if i % 2 == 0:
-            a = policy1(board)
-            board[a] = 1
+            a = policy1(env)
+            env.board[a] = 1
         else:
-            a = policy2(board)
-            board[a] = 2
+            a = policy2(env)
+            env.board[a] = 2
 
         if to_print:
-            print_board(board)
+            print_board(env.board)
 
-        b = is_win(board)
+        b = is_win(env.board)
         if b:
             result = b
             break
@@ -110,14 +144,14 @@ if not"ex1":
         play(policy_random) for i in range(10000))
 
 class Greedy(object):
-    def __init__(self):
-        self.Qtable = init_Q()
+    def __init__(self, QClass=QRaw):
+        self.Qtable = QRaw()
 
     def __call__(self, env):
         from random import choice
         s = board_to_int(env.board)
         actions = get_available_actions(env)
-        qa = [(self.Qtable[s * 9 + a], a) for a in actions]
+        qa = [(self.Qtable.get(s, a), a) for a in actions]
         bestQ, bestA = max(qa)
         bextQ, bestA = choice([(q, a) for (q, a) in qa if q == bestQ])
         return bestA
@@ -145,9 +179,6 @@ class EpsilonGreedy(object):
         return bestA
 
 
-def board_to_state(board):
-    return board_to_int(board)
-
 
 def sarsa(alpha, policyClass=Greedy):
     global environment, policy
@@ -156,33 +187,56 @@ def sarsa(alpha, policyClass=Greedy):
     environment = Environment()
     policy = policyClass()
     action = policy(environment)
-    state = board_to_state(environment.board)
+    state = policy.Qtable.board_to_state(environment.board)
+    prev_num_battle = 0
+    win_ratios = []
     while True:
         next_env, reward = environment(action)
-        next_state = board_to_state(next_env.board)
+        next_state = policy.Qtable.board_to_state(next_env.board)
 
         # determine a'
         next_action = policy(next_env)
-        nextQ = policy.Qtable[next_state * 9 + next_action]
+        nextQ = policy.Qtable.get(next_state, next_action)
 
         # update Q(s, a)
         s_a = state * 9 + action
-        Qsa = policy.Qtable[s_a]
+        Qsa = policy.Qtable.get(state, action)
         estimated_reward = reward + gamma * nextQ
         diff = estimated_reward - Qsa
-        policy.Qtable[s_a] += alpha * diff
+        policy.Qtable.set(state, action, Qsa + alpha * diff)
 
         state = next_state
         action = next_action
-        if len(environment.result_log) == num_result:
-            break
 
+        num_battle = len(environment.result_log)
+        if num_battle > prev_num_battle:
+            if num_battle % 10 == 0:
+                c = Counter(
+                    play(policy, policy_random) for i in range(100))
+                win_ratios.append(float(c[1]) / 100)
+        if num_battle == num_result:
+            break
+        prev_num_battle = num_battle
+
+    #vs = []
+    #for i in range(num_batch):
+    #    c = Counter(environment.result_log[batch_width * i : batch_width * (i + 1)])
+    #    print c
+    #    vs.append(float(c[1]) / batch_width)
+    #return vs
+
+    return win_ratios
+
+def calc_win_ratios_from_result_log(env):
     vs = []
     for i in range(num_batch):
-        c = Counter(environment.result_log[batch_width * i : batch_width * (i + 1)])
+        c = Counter(environment.result_log[
+            batch_width * i :
+            batch_width * (i + 1)])
         print c
         vs.append(float(c[1]) / batch_width)
     return vs
+
 
 def get_Qkey(state, action):
     return state * 9 + action
@@ -195,26 +249,34 @@ def qlearn(alpha, policyClass=Greedy):
     environment = Environment()
     policy = policyClass()
 
-    state = board_to_state(environment.board)
+    state = policy.Qtable.board_to_state(environment.board)
+    prev_num_battle = 0
+    win_ratios = []
     while True:
         action = policy(environment)
         next_env, reward = environment(action)
-        next_state = board_to_state(next_env.board)
+        next_state = policy.Qtable.board_to_state(next_env.board)
 
         # update Q(s, a)
-        maxQ = max(policy.Qtable[get_Qkey(next_state, a)]
+        maxQ = max(policy.Qtable.get(next_state, a)
                    for a in get_available_actions(next_env))
-        s_a = get_Qkey(state, action)
 
-        Qsa = policy.Qtable[s_a]
+        Qsa = policy.Qtable.get(state, action)
         estimated_reward = reward + gamma * maxQ
         diff = estimated_reward - Qsa
-        policy.Qtable[s_a] += alpha * diff
+        policy.Qtable.set(state, action, Qsa + alpha * diff)
 
         state = next_state
 
-        if len(environment.result_log) == num_result:
+        num_battle = len(environment.result_log)
+        if num_battle > prev_num_battle:
+            if num_battle % 10 == 0:
+                c = Counter(
+                    play(policy, policy_random) for i in range(100))
+                win_ratios.append(float(c[1]) / 100)
+        if num_battle == num_result:
             break
+        prev_num_battle = num_battle
 
     vs = []
     for i in range(num_batch):
@@ -241,21 +303,13 @@ def plot(seq, name, baseline=None):
     plt.legend(loc = 4)
     plt.savefig(name)
 
-if 0:
-    result = [
-        (qlearn(0.5), "Qlearn(0.5)"),
-        (qlearn(0.05), "Qlearn(0.05)"),
-        (qlearn(0.005), "Qlearn(0.005)")
-    ]
-    plot(result, 'qlearn.png', baseline=0.58)
 
-
-batch_width = 1000
-num_batch = 1000
+batch_width = 10
+num_batch = 100
 result = [
-    (sarsa(0.05, EpsilonGreedy), "Sarsa(0.05, eps=0.1)"),
-    (qlearn(0.05, EpsilonGreedy), "Qlearn(0.05, eps=0.1)"),
-#    (sarsa(0.05), "Sarsa(0.05)"),
+    (sarsa(0.05), "Sarsa(0.05)"),
+    (sarsa(0.05, lambda: Greedy(QSym)), "Sarsa(0.05)+Sym"),
 #    (qlearn(0.05), "Qlearn(0.05)"),
+#    (qlearn(0.05, lambda: Greedy(QSym)), "Qlearn(0.05)+Sym"),
 ]
 plot(result, 'out.png', baseline=0.58)
